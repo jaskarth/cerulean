6a685d14939dae58920b9cb176c909f3d971aa8a
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-12-17 19:12:59 -0500

    Initial commit

---
+++ src/main/java/com/jaskarth/cerulean/Cerulean.java
@@ -0,0 +1,10 @@
+package com.jaskarth.cerulean;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cerulean implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/jaskarth/cerulean/client/CeruleanClient.java
@@ -0,0 +1,10 @@
+package com.jaskarth.cerulean.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CeruleanClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
